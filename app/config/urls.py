234738e4f084eadaa9a3be8e9ff55d1049205104
Jigor <sloy425@gmail.com>
2025-09-10 00:35:01 +0300

    +lambda render urls

--- app/config/urls.py
+++ app/config/urls.py
@@ -2,7 +2,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+
+from django.shortcuts import render
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', include('accounts.urls')),
+    path('', lambda request: render(request, 'home.html')),
+    path('404/', lambda request: render(request, '404.html')),
+    path('about/', lambda request: render(request, 'about.html')),
+    path('portfolio/', lambda request: render(request, 'portfolio.html')),
 ]
